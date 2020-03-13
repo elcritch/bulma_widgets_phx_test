db68b1a36a57fa533d8939025135b4a34c784873
Jaremy Creechley <creechley@gmail.com>
2020-03-13 10:42:06 -0600

    add example live view

---
+++ lib/bulma_widgets_phx_test_web/live/main_live.ex
@@ -0,0 +1,14 @@
+defmodule BulmaWidgetsPhxTestWeb.MainLive do
+
+  use Phoenix.LiveView
+
+  def mount(_params, _session, socket) do
+    {:ok, socket}
+  end
+
+  def render(assigns) do
+    ~L"""
+    <h1>LiveView is awesome!</h1>
+    """
+  end
+end
--- lib/bulma_widgets_phx_test_web/router.ex
+++ lib/bulma_widgets_phx_test_web/router.ex
@@ -17,6 +17,7 @@ defmodule BulmaWidgetsPhxTestWeb.Router do
     pipe_through :browser
 
     get "/", PageController, :index
+    live "/gallery", MainLive
   end
 
   # Other scopes may use custom stacks.
